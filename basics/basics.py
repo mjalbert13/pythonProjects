17eadf4145708c395d8f063292d89f698308e6c3
mjalbert13 <mjalbert13@gmail.com>
2020-01-30 13:49:46 -0500

    adding

--- basics/basics.py
+++ basics/basics.py
@@ -1,2 +1,11 @@
 import datetime
-print(datetime.datetime.now())
+x = datetime.datetime.now()
+print("The date and time is: ", x)
+
+studentGrades =[60,85,98,80]
+mysum =sum(studentGrades)
+length = len(studentGrades)
+
+mean = mysum/length
+
+print(mean)
